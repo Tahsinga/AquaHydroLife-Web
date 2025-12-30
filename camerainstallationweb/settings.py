d8f8d5ceb64fd028ee1cb1aae42b8e405b45cf11
Tahsinga <munqitshwatashinga1@gmail.com>
2025-12-30 22:42:28 +0200

    Update ALLOWED_HOSTS to use environment variable

--- camerainstallationweb/settings.py
+++ camerainstallationweb/settings.py
@@ -30,18 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-hl!f4)ir@a8iq!4zwxr8r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = [
-    'aquahydrolife.onrender.com',
-    'security-systems.onrender.com',
-    '192.168.1.103',
-    'securty-systems.onrender.com',
-    'localhost',
-    '127.0.0.1',
-    '192.168.1.20',
-    'topsystems.co.zw',
-    'www.topsystems.co.zw',
-    'testserver',
-]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Application definition
 
